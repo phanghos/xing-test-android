611480cf7e69778eb471d4f6881b1472fc4db333
Roberto Tatasciore <rrtatasciore@opentrends.net>
2018-01-19 22:36:58 +0100

    Add main activity, adapter and layouts

---
+++ app/src/main/java/com/taitascioredev/android/xingtest/presentation/adapter/RepositoryAdapter.kt
@@ -0,0 +1,62 @@
+package com.taitascioredev.android.xingtest.presentation.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.jakewharton.rxbinding2.view.RxView
+import com.taitascioredev.android.xingtest.R
+import com.taitascioredev.android.xingtest.data.entity.RepositoryEntity
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
+
+/**
+ * Created by rrtatasciore on 18/01/18.
+ */
+class RepositoryAdapter(private val repos: List<RepositoryEntity>) : RecyclerView.Adapter<RepositoryAdapter.RepositoryViewHolder>() {
+
+    private val longClickSubject = PublishSubject.create<RepositoryEntity>()
+
+    override fun getItemCount(): Int {
+        return repos.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RepositoryViewHolder {
+        val v = LayoutInflater.from(parent?.context).inflate(R.layout.repository_row_layout, parent, false)
+        return RepositoryViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: RepositoryViewHolder?, position: Int) {
+        val repo = repos[position]
+        holder?.let {
+            with (holder) {
+                repoName.text = repo.name
+                repoDescription.text = repo.description
+                repoOwner.text = repo.owner.login
+            }
+        }
+    }
+
+    fun add(repos: List<RepositoryEntity>) {
+        ArrayList<RepositoryEntity>(repos).forEach {
+            (this.repos as ArrayList).add(it)
+            notifyItemInserted(itemCount)
+        }
+    }
+
+    fun getLongClickObservable(): Observable<RepositoryEntity> = longClickSubject
+
+    inner class RepositoryViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+
+        val repoName: TextView = v.findViewById(R.id.tv_repo_name)
+
+        val repoDescription: TextView = v.findViewById(R.id.tv_repo_description)
+
+        val repoOwner: TextView = v.findViewById(R.id.tv_repo_owner)
+
+        init {
+            RxView.longClicks(v).subscribe { longClickSubject.onNext(repos[adapterPosition]) }
+        }
+    }
+}
---
+++ app/src/main/java/com/taitascioredev/android/xingtest/presentation/view/MainActivity.kt
@@ -0,0 +1,124 @@
+package com.taitascioredev.android.xingtest.presentation.view
+
+import android.app.Activity
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View
+import com.jakewharton.rxbinding2.support.v7.widget.RxRecyclerView
+import com.taitascioredev.android.xingtest.R
+import com.taitascioredev.android.xingtest.data.entity.RepositoryEntity
+import com.taitascioredev.android.xingtest.navigateToUrl
+import com.taitascioredev.android.xingtest.presentation.adapter.RepositoryAdapter
+import com.taitascioredev.android.xingtest.presentation.model.RepositoryListViewState
+import com.taitascioredev.android.xingtest.presentation.viewmodel.RepositoryListViewModel
+import com.taitascioredev.android.xingtest.presentation.viewmodel.RepositoryListViewModelFactory
+import com.taitascioredev.android.xingtest.snackbar
+import dagger.android.AndroidInjection
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
+import io.reactivex.functions.BiFunction
+import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
+
+class MainActivity : AppCompatActivity(), HasActivityInjector {
+
+    companion object {
+        private val UPDATE_OFFSET = 2
+    }
+
+    override fun activityInjector(): AndroidInjector<Activity> {
+        return injector
+    }
+
+    private var isListUpdating = false
+
+    @Inject lateinit var factory: RepositoryListViewModelFactory
+
+    @Inject lateinit var injector: DispatchingAndroidInjector<Activity>
+
+    private var adapter: RepositoryAdapter? = null
+
+    private val viewModel: RepositoryListViewModel by lazy {
+        ViewModelProviders.of(this, factory).get(RepositoryListViewModel::class.java)
+    }
+
+    private val scrollAccumulator = BiFunction<Int, Int, Int> { lastPos, curPos ->
+        if (curPos > lastPos) 1 else 0
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        AndroidInjection.inject(this)
+        viewModel.states().observe(this, Observer { render(it) })
+        viewModel.getXingRepos()
+        bindUiEvents()
+    }
+
+    private fun bindUiEvents() {
+        RxRecyclerView.scrollStateChanges(list)
+                .map { (list.layoutManager as LinearLayoutManager).findLastVisibleItemPosition() }
+                .filter { it >= adapter?.itemCount!! - UPDATE_OFFSET && !isListUpdating }
+                .distinctUntilChanged()
+                //.scan(0, scrollAccumulator)
+                //.filter { it == 1 }
+                .doOnNext {
+                    //isListUpdating = true
+                    //snackbar(container, "Loading... Please wait")
+                }
+                .subscribe()
+                //.subscribe { viewModel.getXingRepos(true) }
+    }
+
+    private fun render(state: RepositoryListViewState?) {
+        if (!state?.loading!!) {
+            isListUpdating = false
+        }
+
+        state?.let {
+            when {
+                state.loading -> renderLoading()
+                state.repos != null -> renderRepos(state.repos)
+                state.error != null -> renderError(state.error)
+            }
+        }
+    }
+
+    private fun renderLoading() {
+        progress_wheel.visibility = View.VISIBLE
+    }
+
+    private fun renderRepos(repos: List<RepositoryEntity>) {
+        list.visibility = View.VISIBLE
+        progress_wheel.visibility = View.GONE
+        if (adapter == null) {
+            adapter = RepositoryAdapter(repos)
+            list.adapter = adapter
+            adapter?.getLongClickObservable()?.subscribe { showDialog(it) }
+        } else {
+            adapter?.add(repos)
+        }
+    }
+
+    private fun renderError(error: Throwable) {
+
+    }
+
+    private fun showDialog(repo: RepositoryEntity) {
+        AlertDialog.Builder(this)
+                .setTitle("Confirmation")
+                .setMessage("What page do you want to open?")
+                .setItems(arrayOf("Repo page", "Owner page"), { _, which ->
+                    when (which) {
+                        0 -> navigateToUrl(repo.htmlUrl)
+                        1 -> navigateToUrl(repo.owner.htmlUrl)
+                    }
+                })
+                .show()
+    }
+}
