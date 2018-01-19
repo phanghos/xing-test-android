2455627c56f0dcd19ace986403a268d03d017f63
Roberto Tatasciore <rrtatasciore@opentrends.net>
2018-01-19 23:31:27 +0100

    Remove unused extension method

--- app/src/main/java/com/taitascioredev/android/xingtest/Extensions.kt
+++ app/src/main/java/com/taitascioredev/android/xingtest/Extensions.kt
@@ -3,8 +3,6 @@ package com.taitascioredev.android.xingtest
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
-import android.support.design.widget.Snackbar
-import android.view.View
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
@@ -13,12 +11,6 @@ import com.google.gson.reflect.TypeToken
  */
 fun Activity.navigateToUrl(url: String?) {
     url?.let { startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url))) }
-}
-
-fun snackbar(container: View, msg: String?, duration: Int = Snackbar.LENGTH_SHORT) {
-    if (container != null && msg != null) {
-        Snackbar.make(container, msg, Snackbar.LENGTH_SHORT).show()
-    }
 }
 
 inline fun <reified T> Gson.fromJson(json: String) = this.fromJson<T>(json, object: TypeToken<T>() {}.type)
