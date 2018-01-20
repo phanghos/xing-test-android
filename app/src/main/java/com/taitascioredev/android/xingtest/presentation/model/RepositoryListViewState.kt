8dbc6ef15583532e9555d54b6b30e4c9e931c948
Roberto Tatasciore <rrtatasciore@opentrends.net>
2018-01-20 11:11:52 +0100

    Refactored view state class as AutoValue class for immutability

--- app/src/main/java/com/taitascioredev/android/xingtest/presentation/model/RepositoryListViewState.kt
+++ app/src/main/java/com/taitascioredev/android/xingtest/presentation/model/RepositoryListViewState.kt
@@ -1,18 +1,26 @@
 package com.taitascioredev.android.xingtest.presentation.model
 
+import com.google.auto.value.AutoValue
 import com.taitascioredev.android.xingtest.data.entity.RepositoryEntity
 
 /**
  * Created by rrtatasciore on 18/01/18.
  */
-class RepositoryListViewState(
-        val loading: Boolean,
-        val repos: List<RepositoryEntity>?,
-        val error: Throwable?
-) {
+@AutoValue
+abstract class RepositoryListViewState {
+
+    abstract fun loading(): Boolean
+
+    abstract fun repos(): List<RepositoryEntity>?
+
+    abstract fun error(): Throwable?
+
     companion object {
-        fun success(repos: List<RepositoryEntity>) = RepositoryListViewState(false, repos, null)
-        fun error(error: Throwable) = RepositoryListViewState(false, null, error)
-        fun inFlight() = RepositoryListViewState(true, null, null)
+        fun create(loading: Boolean, repos: List<RepositoryEntity>?, error: Throwable?): RepositoryListViewState {
+            return AutoValue_RepositoryListViewState(loading, repos, error)
+        }
+        fun success(repos: List<RepositoryEntity>) = create(false, repos, null)
+        fun error(error: Throwable) = create(false, null, error)
+        fun inFlight() = create(true, null, null)
     }
 }
