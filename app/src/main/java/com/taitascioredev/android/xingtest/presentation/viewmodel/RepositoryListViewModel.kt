a26a23cb9602fb5d142109888f7ad84a8148c0f4
Roberto Tatasciore <rrtatasciore@opentrends.net>
2018-01-20 19:39:57 +0100

    Remove unused / commented code

--- app/src/main/java/com/taitascioredev/android/xingtest/presentation/viewmodel/RepositoryListViewModel.kt
+++ app/src/main/java/com/taitascioredev/android/xingtest/presentation/viewmodel/RepositoryListViewModel.kt
@@ -5,16 +5,12 @@ import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import com.taitascioredev.android.xingtest.domain.usecase.GetXingReposUseCase
 import com.taitascioredev.android.xingtest.presentation.model.RepositoryListViewState
-import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.subjects.PublishSubject
 
 /**
  * Created by rrtatasciore on 18/01/18.
  */
 class RepositoryListViewModel(private val useCase: GetXingReposUseCase) : ViewModel() {
-
-    private val stateSubject = PublishSubject.create<RepositoryListViewState>()
 
     private val stateLiveData = MutableLiveData<RepositoryListViewState>()
 
@@ -34,13 +30,12 @@ class RepositoryListViewModel(private val useCase: GetXingReposUseCase) : ViewMo
             return
         }
 
-        //disposables.add(useCase.getXingRepos().subscribe { stateSubject.onNext(it) })
         disposables.add(useCase.getXingRepos()
                 .doOnNext { curState = it }
-                .subscribe { stateLiveData.value = it})
+                .subscribe { stateLiveData.value = it })
     }
 
-    fun states(): LiveData<RepositoryListViewState> = stateLiveData //Observable<RepositoryListViewState> = stateSubject
+    fun states(): LiveData<RepositoryListViewState> = stateLiveData
 
     internal fun setCurrentState(state: RepositoryListViewState) {
         curState = state
