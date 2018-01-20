b9eedc06538db1d09206ffd54e541bfecd4f8722
Roberto Tatasciore <rrtatasciore@opentrends.net>
2018-01-20 19:44:14 +0100

    Some improvements in tests

--- app/src/test/java/com/taitascioredev/android/xingtest/data/repository/impl/GithubRepositoryImplTest.kt
+++ app/src/test/java/com/taitascioredev/android/xingtest/data/repository/impl/GithubRepositoryImplTest.kt
@@ -1,19 +1,22 @@
 package com.taitascioredev.android.xingtest.data.repository.impl
 
+import com.google.gson.Gson
 import com.taitascioredev.android.xingtest.data.entity.RepositoryEntity
-import com.taitascioredev.android.xingtest.data.net.GithubService
 import com.taitascioredev.android.xingtest.data.repository.GithubRepository
+import com.taitascioredev.android.xingtest.data.repository.datasource.CloudRepositoryDataStore
+import com.taitascioredev.android.xingtest.data.repository.datasource.RepositoryDataStoreFactory
+import com.taitascioredev.android.xingtest.fromJson
 import com.taitascioredev.android.xingtest.presentation.model.RepositoryListViewState
 import io.reactivex.Observable
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.Schedulers
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations.initMocks
-import java.util.*
-import io.reactivex.schedulers.Schedulers
-import io.reactivex.android.plugins.RxAndroidPlugins
-import io.reactivex.plugins.RxJavaPlugins
-import org.mockito.Mockito.*
 
 
 /**
@@ -21,30 +24,58 @@ import org.mockito.Mockito.*
  */
 class GithubRepositoryImplTest {
 
-    @Mock lateinit var service: GithubService
+    @Mock lateinit var dataStoreFactory: RepositoryDataStoreFactory
+
+    @Mock lateinit var cloudDataStore: CloudRepositoryDataStore
 
     lateinit var repository: GithubRepository
 
     @Before
     fun setUp() {
         initMocks(this)
-        repository = GithubRepositoryImpl(service)
+        repository = GithubRepositoryImpl(dataStoreFactory)
         RxJavaPlugins.setIoSchedulerHandler { Schedulers.trampoline() }
         RxAndroidPlugins.setInitMainThreadSchedulerHandler { Schedulers.trampoline() }
     }
 
     @Test
-    fun getXingRepos_return_expected_value() {
+    fun getXingRepos_should_return_success_state() {
         // Given
-        val repos: List<RepositoryEntity> = ArrayList()
+        val repo = Gson().fromJson<RepositoryEntity>("{}")
+        val repos = ArrayList<RepositoryEntity>()
+        repos.add(repo)
         val reposObservable = Observable.just<List<RepositoryEntity>>(repos)
-        `when`(service.getXingRepos(1)).thenReturn(reposObservable)
+        `when`(dataStoreFactory.create(1)).thenReturn(cloudDataStore)
+        `when`(cloudDataStore.getXingRepos(1)).thenReturn(reposObservable)
 
         // When
         val resultObservable = repository.getXingRepos()
 
         // Then
-        verify(service).getXingRepos(1)
+        verify(dataStoreFactory).create(1)
+        verify(cloudDataStore).getXingRepos(1)
+        resultObservable.test().assertValueCount(2)
+        resultObservable.test().assertValueAt(0, RepositoryListViewState.inFlight())
+        resultObservable.test().assertValueAt(1, RepositoryListViewState.success(repos))
+        resultObservable.test().assertComplete()
+        resultObservable.test().assertNoErrors()
+    }
+
+    @Test
+    fun getXingRepos_should_return_empty_state() {
+        // Given
+        val repos = ArrayList<RepositoryEntity>()
+        val reposObservable = Observable.just<List<RepositoryEntity>>(repos)
+        `when`(dataStoreFactory.create(1)).thenReturn(cloudDataStore)
+        `when`(cloudDataStore.getXingRepos(1)).thenReturn(reposObservable)
+
+        // When
+        val resultObservable = repository.getXingRepos()
+
+        // Then
+        verify(dataStoreFactory).create(1)
+        verify(cloudDataStore).getXingRepos(1)
+        resultObservable.test().assertValueCount(2)
         resultObservable.test().assertValueAt(0, RepositoryListViewState.inFlight())
         resultObservable.test().assertValueAt(1, RepositoryListViewState.success(repos))
         resultObservable.test().assertComplete()
